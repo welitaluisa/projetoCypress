ac768f16be4537053fd2bdc7b2736a9082ad4f49
welitaluisa <welita.luisa@gmail.com>
2022-08-17 22:10:39 -0300

    aula cypress API

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
-const {defineConfig} = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        setupNodeEvents(on, error) {
+        baseUrl: 'https://conexaoqa.herokuapp.com',
+        viewportHeight: 1080,
+        viewportWidth: 1920,
+        // eslint-disable-next-line
+        setupNodeEvents(on, config) {
             // implement node event listeners here
         },
     },
